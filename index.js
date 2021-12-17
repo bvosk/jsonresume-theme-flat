6b3f2d5cc5795dd48f5cbcb196935b3683a6c7ae
Brian Voskerijian <brian.voskerijian@7factor.io>
2021-12-16 20:13:56 -0500

    Fix dev workflow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
-var dateFormat = require('dateformat')
+var dateFormat = require('dateformat');
+var _ = require('underscore');
 
 module.exports = {
 	render: render
@@ -24,4 +25,23 @@ Handlebars.registerHelper("formatDate", function(date, mask) {
 		return 'Present';
 	}
 	return dateFormat(date, mask);
+});
+
+// Thanks to this dev for this snippet: https://jaketrent.com/post/every-nth-item-in-handlebars-loop
+Handlebars.registerHelper('everyNth', function(context, every, options) {
+  var fn = options.fn, inverse = options.inverse;
+  var ret = "";
+  if(context && context.length > 0) {
+    for(var i=0, j=context.length; i<j; i++) {
+      var modZero = i % every === 0;
+      ret = ret + fn(_.extend({}, context[i], {
+        isModZero: modZero,
+        isModZeroNotFirst: modZero && i > 0,
+        isLast: i === context.length - 1
+      }));
+    }
+  } else {
+    ret = inverse(this);
+  }
+  return ret;
 });
